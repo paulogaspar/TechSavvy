f992d9ebef4bd699af5f13afdc1d36527cf54bdc
Paulo Gaspar <anoyzz@gmail.com>
2013-11-14 22:14:44 +0000

    Post image and info are now showing.

--- control.js
+++ control.js
@@ -5,17 +5,12 @@ tsApp.config(function($interpolateProvider){
     $interpolateProvider.endSymbol(']]');
 });
 
-
 tsApp.controller('tsController', function($scope, $http)
 {
 	$scope.allContent = "Loading content..."
 
-	$http.get('http://localhost:8080/getcontent/json').success(function(response) {
-        console.log(response[0]["picture"]);
-        $scope.allContent = response;
+	$http.get('http://localhost:8080/getcontent/json')
+		 .success(function(response) {
+        		$scope.allContent = response;
     });
-
-       /*.then(function(res){
-          $scope.allContent = res.data;                
-        });*/
 });
