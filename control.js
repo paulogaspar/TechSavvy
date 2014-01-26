479ac050dcf489f9a43fc967e11472a75a7870b5
Paulo <anoyzz@gmail.com>
2014-01-26 13:50:21 +0000

    Added URL verification function.

--- control.js
+++ control.js
@@ -48,4 +48,23 @@ tsApp.controller('tsController', function($scope, $http, $window, $timeout)
         restrict: 'A',
         link: linkFn
     };
-});;
+});
+
+
+isURL = function(url) {
+    var strRegex = "^((https|http|ftp|rtsp|mms)?://)"
+        + "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" //ftp?user@
+        + "(([0-9]{1,3}\.){3}[0-9]{1,3}" // IP???URL- 199.194.52.184
+        + "|" // ??IP?DOMAIN(??)
+        + "([0-9a-z_!~*'()-]+\.)*" // ??- www.
+        + "([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]\." // ????
+        + "[a-z]{2,6})" // first level domain- .com or .museum
+        + "(:[0-9]{1,4})?" // ??- :80
+        + "((/?)|" // a slash isn't required if there is no file name
+        + "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$";
+		
+	/* /(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:;,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:;,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:;,.]*\)|[A-Z0-9+&@#\/%=~_|$])/i
+	*/
+    var re=new RegExp(strRegex);
+    return re.test(url.trim());
+}
