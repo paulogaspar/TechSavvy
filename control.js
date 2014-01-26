5dae48bb5eb7d099a1924be1cee8efc9466a9195
Eriksson Monteiro <eriksson.monteiro@ua.pt>
2014-01-26 14:54:13 +0000

    fix post header

--- control.js
+++ control.js
@@ -12,6 +12,26 @@ tsApp.controller('tsController', function($scope, $http, $window, $timeout)
 	$scope.go = function(){
 		$window.open( "https://www.facebook.com/groups/tech.savvyness/permalink/"+ this.post.id.split('_')[1]+"/" );
 	}
+    $scope.isURL = function(url) {
+        if(url==undefined)
+            return false;
+        
+        var strRegex = "^((https|http|ftp|rtsp|mms)?://)"
+            + "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" //ftp?user@
+            + "(([0-9]{1,3}\.){3}[0-9]{1,3}" // IP???URL- 199.194.52.184
+            + "|" // ??IP?DOMAIN(??)
+            + "([0-9a-z_!~*'()-]+\.)*" // ??- www.
+            + "([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]\." // ????
+            + "[a-z]{2,6})" // first level domain- .com or .museum
+            + "(:[0-9]{1,4})?" // ??- :80
+            + "((/?)|" // a slash isn't required if there is no file name
+            + "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$";
+            
+        /* /(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:;,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:;,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:;,.]*\)|[A-Z0-9+&@#\/%=~_|$])/i
+        */
+        var re=new RegExp(strRegex);
+        return re.test(url.trim());
+    }
 
 	$http.get('/content/json')
 		 .success(function(response) {
@@ -49,22 +69,3 @@ tsApp.controller('tsController', function($scope, $http, $window, $timeout)
         link: linkFn
     };
 });
-
-
-isURL = function(url) {
-    var strRegex = "^((https|http|ftp|rtsp|mms)?://)"
-        + "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" //ftp?user@
-        + "(([0-9]{1,3}\.){3}[0-9]{1,3}" // IP???URL- 199.194.52.184
-        + "|" // ??IP?DOMAIN(??)
-        + "([0-9a-z_!~*'()-]+\.)*" // ??- www.
-        + "([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]\." // ????
-        + "[a-z]{2,6})" // first level domain- .com or .museum
-        + "(:[0-9]{1,4})?" // ??- :80
-        + "((/?)|" // a slash isn't required if there is no file name
-        + "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$";
-		
-	/* /(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:;,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:;,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:;,.]*\)|[A-Z0-9+&@#\/%=~_|$])/i
-	*/
-    var re=new RegExp(strRegex);
-    return re.test(url.trim());
-}
